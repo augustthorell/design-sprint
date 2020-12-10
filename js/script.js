894c38c1d2a156b92d8f8ba32938aca5a93cafe0
August Thorell <augustthorell@gmail.com>
2020-12-10 08:24:05 +0100

    Added images and cleaned up the layout

--- js/script.js
+++ js/script.js
@@ -1,9 +1,34 @@
+let loginIcon = document.getElementById('login-icon');
+let loginText = document.getElementById('login-text');
+let modal = document.querySelector('.modal-wrapper');
+let closeModalBtn = document.querySelector('.close-modal');
+let cancelBtnModal = document.querySelector('.cancelbtn');
+
+
 window.onscroll = function() {
     if(window.pageYOffset >= 500) {
-        
         document.querySelector('.go-to-top').style.visibility="visible";
     }else {
         document.querySelector('.go-to-top').style.visibility="hidden";
     }
 };
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+loginIcon.addEventListener('click', openModal);
+loginText.addEventListener('click', openModal);
+closeModalBtn.addEventListener('click', closeModal)
+cancelBtnModal.addEventListener('click', closeModal)
+
+function openModal() {
+    modal.style.display = 'block'
+}
+function closeModal() {
+    modal.style.display = 'none'
+}
 
